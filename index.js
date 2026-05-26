c38da98ca18ff7231e9ddf7b4b0d9352d09114cd
Swarnalatha Bodicharla <swarnalatha200512@gmail.com>
2026-05-26 11:40:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+function AWS() {
+    window.location.href = "./images/AWS.jpg";
+}
+
+function CYNOSURE() {
+    window.location.href = "./images/CYNOSURE.jpg";
+}
+
+function CODZEN() {
+    window.location.href = "./images/CODZEN.jpg";
+}
+
+function DEVOPS() {
+    window.location.href = "./images/DEVOPS.jpg";
+}
+
+function MERIT() {
+    window.location.href = "./images/MERIT.jpg";
+}
+
+function WADHWANI() {
+    window.location.href = "./images/WADHWANI.png";
+}
